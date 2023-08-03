10754a5380ee5c8c34cee15abb412f346671dbc4
Stefano-BML <stefanoml1128@gmail.com>
2023-08-02 23:44:13 -0700

    add: os and replace init :rocket:

--- console.py
+++ console.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """ Console Module """
 import cmd
+import os
 import sys
 from models.base_model import BaseModel
-from models.__init__ import storage
+from models import storage
 from models.user import User
 from models.place import Place
 from models.state import State
