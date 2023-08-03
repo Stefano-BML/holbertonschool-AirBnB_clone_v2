42fbbba1d635e73a77fe4034e7d19bd2028b4f50
Nilesoj20 <jvrn2014@gmail.com>
2023-08-03 16:13:56 -0500

    fix: creating the table and user for the project

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,7 @@
 -- prepares a MySQL server for the project
--- reate database, user with privileges
-CREATE SCHEMA IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
-FLUSH PRIVILEGES;
+-- Create database, user with privileges
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_dev'@'localhost';
