feaae4f734571a0f4ddef6b53a440e452c53a212
Nilesoj20 <jvrn2014@gmail.com>
2023-08-05 10:48:09 -0700

    fix: adding lines to correct the syntax

--- models/user.py
+++ models/user.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey, Integer
 from os import environ
 
 
-if environ.get('HBNB_TYPE_STORAGE') == 'db'
+if environ.get('HBNB_TYPE_STORAGE') == 'db':
+
     class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
+        """defines the attributes to be stored in the DB"""
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
+        first_name = Column(String(128))
+        last_name = Column(String(128))
+        places = relationship("Place", backref="user",
+                              cascade='all, delete')
+        reviews = relationship('Review', backref='user',
+                               cascade='all, delete')
 else:
     class User(BaseModel):
-        """ Comentario """
+        """defines the attributes to be stored in Json"""
         email = ''
         password = ''
         first_name = ''
