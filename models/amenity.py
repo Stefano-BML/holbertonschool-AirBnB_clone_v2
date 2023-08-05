04bc76fb1bd8a991fc209e18762c9619fa2e634b
Stefano Madueño <87108488+Stefano-BML@users.noreply.github.com>
2023-08-05 18:09:44 -0500

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -10,8 +10,7 @@ class Amenity(BaseModel, Base):
     """ Amenity: Class"""
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    # place_amenities = relationship("Place", secondary="place_amenity",
-                                   # viewonly=False)
+    place_amenities = relationship("Place", secondary="place_amenity", viewonly=False)
 
     def __init__(self, *args, **kwargs):
         """
