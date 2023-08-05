e2758c6e5acdb402ec1d6f6d34fbe9121a108b67
Stefano Madueño <87108488+Stefano-BML@users.noreply.github.com>
2023-08-05 16:41:54 -0500

    Update place.py

--- models/place.py
+++ models/place.py
@@ -19,6 +19,7 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, nullable=False, default=0)
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
+        reviews = relationship("Review", backref="place", cascade="delete")
 
     else:
         """ defines the attributes to be stored in the JSON """
