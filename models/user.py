2dc318caccb23e10f74356e8ae75c20b1ccbeb9e
Nilesoj20 <jvrn2014@gmail.com>
2023-08-05 10:42:18 -0700

    fix: correct the structure to prepare according to the type of storage

--- models/user.py
+++ models/user.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, String
+from os import environ
 
 
-class User(BaseModel, Base):
+if environ.get('HBNB_TYPE_STORAGE') == 'db'
+    class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
+else:
+    class User(BaseModel):
+        """ Comentario """
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
