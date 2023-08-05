4c1a1c79b133579a758e0b6dc91840268bc50183
Nilesoj20 <jvrn2014@gmail.com>
2023-08-05 13:22:30 -0700

    fix: creating the attributes for the Please classes and creating the relationship between the Place, city and user tables

--- models/place.py
+++ models/place.py
@@ -1,18 +1,34 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Table, String, Integer, Float, ForeignKey
+import os
 
 
-class Place(BaseModel):
-    """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
+class Place(BaseModel, Base):
+    """ defines the attributes to be stored in the DB """
+    if environ.get('HBNB_TYPE_STORAGE') == 'db':
+    __tablename__ = 'places'
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name =  Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=False)
+    number_rooms = Column(Integer(), nullable=False, default=0)
+    number_bathrooms = Column(Integer(), nullable=False, default=0)
+    max_guest = Column(Integer(), nullable=False, default=0)
+    price_by_night = Column(Integer(), nullable=False, default=0)
+    latitude = Column(Float(), nullable=False)
+    longitude = Column(Float(), nullable=False)
+    else:
+        """ defines the attributes to be stored in the JSON """
+        city_id = ''
+        user_id = ''
+        name = ''
+        description = ''
+        number_rooms = ''
+        number_bathrooms = ''
+        max_guest = ''
+        price_by_night = ''
+        latitude = ''
+        longitude = ''
+        amenity_ids = []
--- models/user.py
+++ models/user.py
@@ -2,12 +2,24 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from models.place import Place
+from sqlalchemy.orm import relationship
+import os
 
 
 class User(BaseModel, Base):
     """defines the attributes to be stored in the DB"""
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
     __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
+    places = relationship("Place", backref="user",
+                          cascade='all, delete')
+    else:
+        """defines the attributes to be stored in the JSON"""
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
