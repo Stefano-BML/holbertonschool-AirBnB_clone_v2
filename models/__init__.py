2d070d3996c73a2c607c1bfed608e9df5a35153a
Stefano Madueño <87108488+Stefano-BML@users.noreply.github.com>
2023-08-05 21:34:24 -0500

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 import os
+from models.state import State
+from models.city import City
+from models.user import User
+from models.place import Place
+from models.amenity import Amenity
 
 storage_type = os.getenv('HBNB_TYPE_STORAGE')
 
