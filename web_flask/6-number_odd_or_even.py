f17714ffc22a0b680a5d9615322fb47db481afc2
Stefano-BML <stefanoml1128@gmail.com>
2023-08-15 05:29:18 -0700

    feat: number odd or even :rocket:

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python3
+"""
+A Flask web application with routes to display greetings, custom messages, number validation, and HTML templates.
+"""
+
+from flask import Flask, render_template_string
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Route handler for the root route.
+
+    Returns:
+        str: The greeting message "Hello HBNB!".
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Route handler for the '/hbnb' route.
+
+    Returns:
+        str: The message "HBNB".
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def custom_message(text):
+    """
+    Route handler for the '/c/<text>' route.
+
+    Args:
+        text (str): The text variable extracted from the URL.
+
+    Returns:
+        str: The message "C " followed by the value of the text variable
+             with underscores replaced by spaces.
+    """
+    return "C " + text.replace('_', ' ')
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_message(text):
+    """
+    Route handler for the '/python/<text>' route.
+
+    Args:
+        text (str): The text variable extracted from the URL.
+
+    Returns:
+        str: The message "Python " followed by the value of the text variable
+             with underscores replaced by spaces.
+    """
+    return "Python " + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_check(n):
+    """
+    Route handler for the '/number/<n>' route.
+
+    Args:
+        n (int): The integer value extracted from the URL.
+
+    Returns:
+        str: The message "{n} is a number".
+    """
+    return f"{n} is a number"
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """
+    Route handler for the '/number_template/<n>' route.
+
+    Args:
+        n (int): The integer value extracted from the URL.
+
+    Returns:
+        str: An HTML page displaying "Number: n" using a template.
+    """
+    return render_template('5-number.html', number=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    """
+    Route handler for the '/6-number_odd_or_even.html/<n>' route.
+
+    Args:
+        n (int): The integer value extracted from the URL.
+
+    Returns:
+        str: An HTML page displaying "Number: n is even|odd" using a template.
+    """
+    return render_template('6-number_odd_or_even.html',
+                number=n, even_odd='even' if n % 2 == 0 else 'odd')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
