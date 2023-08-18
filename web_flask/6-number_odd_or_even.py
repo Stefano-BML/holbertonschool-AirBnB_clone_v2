18bab50295595b6b3de6ac0a71e921d949ce8fb8
Stefano-BML <stefanoml1128@gmail.com>
2023-08-18 12:37:19 -0700

    fix: another idea .py

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,107 +1,62 @@
 #!/usr/bin/python3
-"""
-A Flask web application
-"""
-
-from flask import Flask, render_template_string
+"""6-number_odd_or_even Module"""
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """
-    Route handler for the root route.
-
-    Returns:
-        str: The greeting message "Hello HBNB!".
-    """
+def hello_page():
+    """Displays 'Hello HBNB!' upon visiting root page"""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """
-    Route handler for the '/hbnb' route.
-
-    Returns:
-        str: The message "HBNB".
-    """
+def hbnb_page():
+    """Displays 'HBNB' upon visiting /hbnb page"""
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def custom_message(text):
-    """
-    Route handler for the '/c/<text>' route.
-
-    Args:
-        text (str): The text variable extracted from the URL.
-
-    Returns:
-        str: The message "C " followed by the value of the text variable
-             with underscores replaced by spaces.
-    """
-    return "C " + text.replace('_', ' ')
+def C_page(text):
+    """Displays 'C (text variable with spaces instead of _'s)'
+        upon visiting /c/<text> page
+        (text is optional)"""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
 
 
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/')
 @app.route('/python/<text>', strict_slashes=False)
-def python_message(text):
-    """
-    Route handler for the '/python/<text>' route.
-
-    Args:
-        text (str): The text variable extracted from the URL.
-
-    Returns:
-        str: The message "Python " followed by the value of the text variable
-             with underscores replaced by spaces.
-    """
-    return "Python " + text.replace('_', ' ')
+def python_page(text="is cool"):
+    """Displays 'Python (text variable with spaces instead of _'s)'
+        upon visiting /python/<text> page
+        (text default = "is cool")"""
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def number_check(n):
-    """
-    Route handler for the '/number/<n>' route.
-
-    Args:
-        n (int): The integer value extracted from the URL.
-
-    Returns:
-        str: The message "{n} is a number".
-    """
-    return f"{n} is a number"
+def number_page(n):
+    """Displays '(Variable n) is a number'
+        upon visiting /number/(number) page
+        if n is really a number."""
+    return "{} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    """
-    Route handler for the '/number_template/<n>' route.
-
-    Args:
-        n (int): The integer value extracted from the URL.
-
-    Returns:
-        str: An HTML page displaying "Number: n" using a template.
-    """
-    return render_template('5-number.html', number=n)
+def number_template_page(n):
+    """Displays html page
+        if n is really a number."""
+    return render_template('5-number.html', n=n)
 
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def number_odd_or_even(n):
-    """
-    Route handler for the '/6-number_odd_or_even.html/<n>' route.
+def number_odd_even_page(n):
+    """Displays html page
+        if n is really a number."""
+    return render_template('6-number_odd_or_even.html', n=n)
 
-    Args:
-        n (int): The integer value extracted from the URL.
-
-    Returns:
-        str: An HTML page displaying "Number: n is even|odd" using a template.
-    """
-    even_odd = 'even' if n % 2 == 0 else 'odd'
-    return render_template('6-number_odd_or_even.html', number=n, even_odd=even_odd)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0")
